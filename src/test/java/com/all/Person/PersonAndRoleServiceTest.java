380914679c3c939dec6ec5c28b562588bba12657
alexanderkroshev <alexanderkroshev@gmail.com>
2021-10-13 20:14:33 +0700

    completed

--- src/test/java/com/all/Person/PersonAndRoleServiceTest.java
+++ src/test/java/com/all/Person/PersonAndRoleServiceTest.java
@@ -1,39 +1,18 @@
 package com.all.Person;
 
 import com.all.Role.Role;
-import com.all.Role.RoleRepository;
-import org.checkerframework.checker.units.qual.A;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 
@@ -52,10 +31,6 @@ class PersonAndRoleServiceTest {
         assertThat(person2.getNickname()).isEqualTo(nickname);
     }
 
-    @Before
-    void setUp(){
-     //  дописать события до
-    }
     @Test
     void findRoleByName() {
         String name = "admin";
@@ -65,7 +40,6 @@ class PersonAndRoleServiceTest {
 
     @Test
     void savePerson() {
-
         Person person = new Person();
         String nickname = "Vlad_95hgjgj";
         person.setNickname(nickname);
@@ -80,8 +54,8 @@ class PersonAndRoleServiceTest {
 
     @Test
     void deletePerson() {
-        Person person = service.findPersonByNickname("Vlad_9532");
+        Person person = service.findPersonByNickname("Vlad_95hgjgj");
         this.service.deletePerson(person);
-        assertThat(this.service.findPersonByNickname("Vlad_9532")).isNull();
+        assertThat(this.service.findPersonByNickname("Vlad_95hgjgj")).isNull();
     }
 }
